be7ebdb1bfd9246cc9c5490d16ecf98a8a642a0d
Yash Akotkar <yashakotkar@gmail.com>
2022-04-01 23:50:44 +0530

    Solidity Part Completed

--- src/contracts/CryptoWall.sol
+++ src/contracts/CryptoWall.sol
@@ -2,4 +2,81 @@ pragma solidity ^0.5.16;
 
 contract CryptoWall {
     string public name = "Crypto Wall";
+
+    // Storage Post
+    uint256 public postCount = 0;
+    mapping(uint256 => Post) public posts;
+
+    struct Post {
+        uint256 id;
+        string imageHash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event PostCreated(
+        uint256 id,
+        string imageHash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event PostTipped(
+        uint256 id,
+        string imageHash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Create Posts
+    function createPost(string memory _imgHash, string memory _desc) public {
+        // Make sure image hash exist
+        require(bytes(_imgHash).length > 0);
+
+        // Make sure image desc exist
+        require(bytes(_desc).length > 0);
+
+        // Make sure image desc exist
+        require(msg.sender != address(0x0));
+
+        // Increment Count
+        postCount++;
+
+        // Add Image to contract
+        posts[postCount] = Post(postCount, _imgHash, _desc, 0, msg.sender);
+
+        // Trigger an event
+        emit PostCreated(postCount, _imgHash, _desc, 0, msg.sender);
+    }
+
+    // Tip Posts
+    function tipPostOwner(uint256 _id) public payable {
+        // Make sure ID is valid
+        require(_id > 0 && _id <= postCount);
+        // Fetch Image
+        Post memory _post = posts[_id];
+        // Fetch Author
+        address payable _author = _post.author;
+
+        // Tranfer crypto to Author
+        address(_author).transfer(msg.value);
+
+        // Update tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+
+        // Update Post
+        posts[_id] = _post;
+
+        // Trigger an event
+        emit PostTipped(
+            _id,
+            _post.imageHash,
+            _post.description,
+            _post.tipAmount,
+            _author
+        );
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+const { assert } = require("chai");
+// const web3 = require("web3");
+
 const CryptoWall = artifacts.require("./CryptoWall.sol");
 
 require("chai").use(require("chai-as-promised")).should();
@@ -21,6 +24,87 @@ contract("Crypto Wall", ([deployer, author, tipper]) => {
     it("has a name", async () => {
       const name = await cryptoWall.name();
       assert.equal(name, "Crypto Wall");
+    });
+  });
+
+  describe("posts", async () => {
+    let results, postCount;
+    const hash = "abc123";
+    const desc = "Hello World!";
+
+    before(async () => {
+      results = await cryptoWall.createPost(hash, desc, { from: author });
+      postCount = await cryptoWall.postCount();
+    });
+
+    it("create posts", async () => {
+      // Success
+      assert.equal(postCount, 1);
+      const event = results.logs[0].args;
+      assert.equal(event.id.toNumber(), postCount.toNumber(), "[Id Correct]");
+      assert.equal(event.imageHash, hash, "[Hash Correct]");
+      assert.equal(event.description, desc, "[Description Correct]");
+      assert.equal(event.tipAmount, 0, "[Tip Amount Correct]");
+      assert.equal(event.author, author, "[Author Correct]");
+
+      // Failure: Image Should have hash
+      await cryptoWall.createPost("", "IMG DESC", { from: author }).should.be
+        .rejected;
+
+      // Failure: Image Should have description
+      await cryptoWall.createPost("uefiiofjeofow", "", { from: author }).should
+        .be.rejected;
+    });
+
+    it("list posts", async () => {
+      const post = await cryptoWall.posts(postCount);
+      assert.equal(post.id.toNumber(), postCount.toNumber(), "[Id Correct]");
+      assert.equal(post.imageHash, hash, "[Hash Correct]");
+      assert.equal(post.description, desc, "[Description Correct]");
+      assert.equal(post.tipAmount, 0, "[Tip Amount Correct]");
+      assert.equal(post.author, author, "[Author Correct]");
+    });
+
+    it("allow users to tip posts", async () => {
+      let oldAuthorBalance;
+      oldAuthorBalance = await web3.eth.getBalance(author);
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance);
+
+      let result = await cryptoWall.tipPostOwner(postCount, {
+        from: tipper,
+        value: web3.utils.toWei("1", "Ether"),
+      });
+
+      // Success
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), postCount.toNumber(), "[Id Correct]");
+      assert.equal(event.imageHash, hash, "[Hash Correct]");
+      assert.equal(event.description, desc, "[Description Correct]");
+      assert.equal(
+        event.tipAmount,
+        "1000000000000000000",
+        "[Tip Amount Correct]"
+      );
+      assert.equal(event.author, author, "[Author Correct]");
+
+      // Check author receive funds
+      let newAuthorBalance;
+      newAuthorBalance = await web3.eth.getBalance(author);
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance);
+
+      let tipPostOwner;
+      tipPostOwner = web3.utils.toWei("1", "Ether");
+      tipPostOwner = new web3.utils.BN(tipPostOwner);
+
+      const expectBalance = oldAuthorBalance.add(tipPostOwner);
+
+      assert.equal(expectBalance.toString(), newAuthorBalance.toString());
+
+      // Failure: Tries to tip a image that does not exist
+      await cryptoWall.tipPostOwner(99, {
+        from: tipper,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
     });
   });
 });
